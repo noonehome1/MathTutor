740d22304d6615fc393684cb69b40534756323f0
Paul Wagstaff <paul@pwfotos.ca>
2025-11-23 10:46:53 -0700

    Initial Commit

---
+++ MathTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathTutor
+//
+//  Created by Paul Wagstaff on 2025-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Paul Wagstaff on 2025-11-23.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
